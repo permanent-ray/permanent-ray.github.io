0024108989995fdc5ba8c8c2fe2d18466ec1103f
permanent-ray <2939098516@qq.com>
2025-08-09 17:03:59 +0800

    Site updated: 2025-08-09 17:03:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/09/强袭自由/","2025/08/09/hello-world/","2025/08/09/面朝大海，春暖花开！/"];function toRandomPost(){
+var posts=["2025/08/09/hello-world/","2025/08/09/强袭自由/","2025/08/09/面朝大海，春暖花开！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
