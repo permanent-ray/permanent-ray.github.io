94ac6500942a2d2619047f5f0412ff5b62a6aa43
permanent-ray <2939098516@qq.com>
2025-08-11 11:02:13 +0800

    Site updated: 2025-08-11 11:02:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/11/在心里种花，人生才不会荒芜。/","2025/08/09/面朝大海，春暖花开！/"];function toRandomPost(){
+var posts=["2025/08/09/面朝大海，春暖花开！/","2025/08/11/在心里种花，人生才不会荒芜。/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
