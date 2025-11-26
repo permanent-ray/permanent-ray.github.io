ba63b719b728a0a8e4aa235c4a90762b4488d542
permanent-ray <2939098516@qq.com>
2025-11-26 10:04:58 +0800

    Site updated: 2025-11-26 10:04:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["lief/一个人的朝圣/","lief/法算day9/","lief/法算day8/","lief/法算day7/","lief/法算day6/","lief/法算day5/","lief/法算day4/","lief/法算day3/","lief/法算day2/","lief/法算day1/","lief/Springboot学习日志/","lief/终不似-少年游/","lief/图床搭建-Cloudflare-R2/","lief/在心里种花，人生才不会荒芜。/","lief/面朝大海，春暖花开！/"];function toRandomPost(){
+var posts=["lief/一个人的朝圣/","lief/法算day10/","lief/法算day9/","lief/法算day8/","lief/法算day7/","lief/法算day6/","lief/法算day5/","lief/法算day4/","lief/法算day3/","lief/法算day2/","lief/法算day1/","lief/Springboot学习日志/","lief/终不似-少年游/","lief/图床搭建-Cloudflare-R2/","lief/在心里种花，人生才不会荒芜。/","lief/面朝大海，春暖花开！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
